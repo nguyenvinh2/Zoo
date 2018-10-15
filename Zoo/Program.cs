2b1de228acf685f157983ba81fd1d778e303e023
nguye <nguyenv2@outlook.com>
2018-10-15 09:35:55 -0700

    one override added

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -2,6 +2,61 @@
 
 namespace Zoo
 {
+  abstract class Animal
+  {
+    abstract public string CanItSwim();
+    abstract public int LifeSpan();
+  }
+
+  abstract class Mammal: Animal
+  {
+    public override string CanItSwim()
+    {
+      return "Possibly";
+    }
+  }
+
+  abstract class Fish: Animal
+  {
+    public override string CanItSwim()
+    {
+      return "Yes";
+    }
+  }
+
+  abstract class Aves: Animal
+  {
+    public override string CanItSwim()
+    {
+      return "Yes";
+    }
+  }
+
+  class Dog
+  {
+  }
+
+  class Cat
+  {
+  }
+
+  class Puffer
+  {
+  }
+
+  class Pirahna
+  {
+  }
+
+  class Eagle
+  {
+  }
+
+  class Sparrow
+  {
+  }
+
+
   class Program
   {
     static void Main(string[] args)
